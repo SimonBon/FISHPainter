2605c7e1981b57f7a0773402175db5948be48fd5
SimonBon <simon.bonvino@gmail.com>
2023-12-04 22:04:20 +0100

    deleted unneccecary files

--- FISHcreation/__init__.py
+++ FISHcreation/__init__.py
@@ -1,1 +1,1 @@
-__version__ = 'version 0.1'
+__version__ = '0.5'
--- FISHcreation/src/signals.py
+++ FISHcreation/src/signals.py
@@ -2,7 +2,13 @@ import numpy as np
 from scipy.ndimage import gaussian_filter, map_coordinates, binary_erosion
 from  .process_boxes import merge_boxes_by_labels
 
-def create_signal(image_size, position, signal_size, min_brightness=0.2, max_brightness=1.0):
+MAX_ITERS=100
+
+# debug
+import matplotlib.pyplot as plt
+from cellplot.patches import draw_boxes_on_patch
+
+def create_signal(image_size, position, signal_size, min_brightness=0.5, max_brightness=1.0):
     """
     Creates a Gaussian signal of specified size at a specified position.
 
@@ -19,18 +25,61 @@ def create_signal(image_size, position, signal_size, min_brightness=0.2, max_bri
     signal[position] = 1
     signal = gaussian_filter(signal, sigma=signal_size)
     signal = signal / signal.max()
-    signal[signal > 0.8] = 1.
-    signal[signal < 0.8] = 0.
+    signal[signal < 0.2] = 0.
     
     scaling_factor = np.random.uniform(min_brightness, max_brightness)
     signal *= scaling_factor
 
-    # Obtain bounding box coordinates
+    return signal
+
+def create_cluster(image_size, position, valid_positions, signal_size, cluster_size, min_brightness=0.5, max_brightness=1.0):
+    """
+    Creates a Gaussian signal of specified size at a specified position.
+
+    Parameters:
+    - image_size: Tuple indicating the size of the output image (height, width).
+    - position: Tuple indicating the position (y, x) to place the Gaussian center.
+    - signal_size: Size (standard deviation) of the Gaussian.
+
+    Returns:
+    - A 2D numpy array representing the image with the Gaussian signal.
+    - Bounding box coordinates as a tuple (min_y, min_x, max_y, max_x).
+    """
+    signal = np.zeros(image_size)
+    
+    signal[position] = 1
+    
+    curr_position = position
+    curr_signals = 0
+    iters = 0
+    while curr_signals < cluster_size:
+        
+        iters += 1
+        if iters > MAX_ITERS:
+            break
+        
+        next_position = np.array([np.round(x + np.random.choice([-2*signal_size, +2*signal_size], 1)[0], 0).astype(int) for x in curr_position])
+        
+        if not (np.any(np.all(valid_positions == next_position, axis=1))):
+            continue
+        
+        signal[next_position[0], next_position[1]] = 1
+        
+        curr_position = next_position
+        curr_signals += 1
+        
+    signal = gaussian_filter(signal, sigma=signal_size)
+    
+    signal = signal / signal.max()
+    signal[signal < 0.2] = 0.
+
+    scaling_factor = np.random.uniform(min_brightness, max_brightness)
+    signal *= scaling_factor
 
     return signal
 
 
-def create_FISH(patch, mask, num_red=2, num_green=2, signal_size=3, seed=None, alpha=20, sigma=2, return_as_dict=False):
+def create_FISH(patch, mask, num_red=2, num_green=2, num_green_cluster=0, num_red_cluster=0, signal_size=2, green_cluster_size=4, red_cluster_size=4, seed=None, alpha=20, sigma=2, return_as_dict=False):
     """
     Modifies the input image patch by placing Gaussian dots based on the mask and desired number of red and green dots.
 
@@ -57,7 +106,7 @@ def create_FISH(patch, mask, num_red=2, num_green=2, signal_size=3, seed=None, a
     h, w, _ = patch.shape
     
     # Erode mask
-    structure = np.ones((int(signal_size), int(signal_size)))
+    structure = np.ones((int(2*signal_size), int(2*signal_size)))
     diluted_mask = binary_erosion(mask, structure=structure)
     
     valid_positions = np.argwhere(diluted_mask)
@@ -80,9 +129,33 @@ def create_FISH(patch, mask, num_red=2, num_green=2, signal_size=3, seed=None, a
         patch[..., 1] += green_signal  # Add to green channel
         bboxes.append(bbox)
         labels.append(2)
+        
+    for _ in range(num_green_cluster):
+        
+        start_position = valid_positions[np.random.choice(len(valid_positions))]
+        green_signal = create_cluster((h, w), (start_position[0], start_position[1]), valid_positions, signal_size, green_cluster_size)
+        green_signal, bbox = elastic_transform(green_signal, alpha=alpha, sigma=sigma)
+        patch[..., 1] += green_signal  # Add to green channel
+        bboxes.append(bbox)
+        labels.append(3)
+        
+    for _ in range(num_red_cluster):
+        
+        start_position = valid_positions[np.random.choice(len(valid_positions))]
+        red_signal = create_cluster((h, w), (start_position[0], start_position[1]), valid_positions, signal_size, red_cluster_size)
+        red_signal, bbox = elastic_transform(red_signal, alpha=alpha, sigma=sigma)
+        patch[..., 0] += red_signal  # Add to red channel
+        bboxes.append(bbox)
+        labels.append(4)
 
     patch = np.clip(patch, 0, 1)  # Ensure values are in [0, 1] range
+    
+    #plt.imshow(draw_boxes_on_patch(patch.copy(), bboxes, labels)); plt.show()
+    
     bboxes, labels = merge_boxes_by_labels(bboxes, labels)
+    
+    #plt.imshow(draw_boxes_on_patch(patch.copy(), bboxes, labels)); plt.show()
+
     
     if return_as_dict:
         return dict(patch=patch, bboxes=bboxes, labels=labels)
